563574167a2b14bc1a9426ce7b2227b62481ecb3
Rahul Gupta <rg5353070@gmail.com>
2024-01-14 20:25:13 +0530

    made changes to heading of prose

--- components/ContentBody.tsx
+++ components/ContentBody.tsx
@@ -28,7 +28,7 @@ export default function ContentBody({
         <p className="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">
           {formattedDate}
         </p>
-        <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
+        <div className="prose-xs prose prose-invert mt-12 w-full max-w-none md:prose-lg prose-h1:text-4xl md:mt-20">
           <SliceZone slices={page.data.slices} components={components} />
         </div>
       </div>
