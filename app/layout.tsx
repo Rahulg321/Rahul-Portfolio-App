6b73d28b306fde7ebf6d775be3c9d769b2f32c75
Rahul Gupta <rg5353070@gmail.com>
2024-03-11 00:50:28 +0530

    added twitter and open graph images to the app for meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,22 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-gray-900 text-slate-100">
       <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+        <meta
+          property="og:image:alt"
+          content="Meet Rahul Gupta. Developer from India"
+        />
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
+        <meta
+          property="twitter:image:alt"
+          content="Meet Rahul Gupta. Developer from India"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
