2cc3e1f7dcd4c2f028305c4a8f67c0b01f43f4d9
Rahul Gupta <rg5353070@gmail.com>
2024-04-07 23:23:52 +0530

    added a route handler and middleware for testing

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,23 +5,39 @@ export const dynamic = "force-dynamic";
 
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
-export async function POST() {
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: "Rahul Gupta <rahulGupta@rahulguptadev.in>",
-      to: ["rg5353070@gmail.com"],
-      subject: "hello world",
-      html: "<strong>it works!</strong>",
-    }),
-  });
+export async function POST(request: Request) {
+  const formData = await request.json();
+  const { personEmail, personName, phoneNumber, foodName, address } = formData;
 
-  if (res.ok) {
-    const data = await res.json();
-    return NextResponse.json(data);
+  try {
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: "Rahul Gupta <rahulGupta@rahulguptadev.in>",
+        to: ["pctesanjanabca21b@gmail.com", "rg5353070@gmail.com"],
+        subject: "the request was send from sanjana contact form",
+        html: `<strong>it works!</strong>
+          <ul>
+            <li>from frontend</li>
+            <li>${personName}</li>
+            <li>${personEmail}</li>
+            <li>${foodName}</li>
+            <li>${phoneNumber}</li>
+            <li>${address}</li>
+          </ul>
+        `,
+      }),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      return NextResponse.json(data);
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
