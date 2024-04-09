9a2403adc7cc8a5f05cca8a11a25a513d3f090fd
Rahul Gupta <rg5353070@gmail.com>
2024-04-09 23:12:20 +0530

    changed the api route to orders

---
+++ app/api/orders/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "@/components/firebase/config";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+export async function POST(request: Request) {
+  try {
+    const newCityRef = doc(collection(db, "orders"));
+
+    // later...
+    await setDoc(newCityRef, {
+      name: "orders",
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Order added successfully!",
+    });
+  } catch (error) {
+    console.error("Error adding Order:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to add Order." },
+      { status: 500 },
+    );
+  }
+}
