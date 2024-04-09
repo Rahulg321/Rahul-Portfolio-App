b0863384e536dd566bffd8ef2338d0f634f4d9b6
Rahul Gupta <rg5353070@gmail.com>
2024-04-09 23:25:59 +0530

    send api change

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,6 +4,30 @@ export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "@/components/firebase/config";
+
+export async function GET(request: Request) {
+  try {
+    const newCityRef = doc(collection(db, "orders"));
+
+    // later...
+    await setDoc(newCityRef, {
+      name: "orders",
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Order added successfully!",
+    });
+  } catch (error) {
+    console.error("Error adding Order:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to add Order." },
+      { status: 500 },
+    );
+  }
+}
 
 export async function POST(request: Request) {
   const formData = await request.json();
